205c41adfbdc1eba09e5d22df91f877c6af5ea57
Tero Saarni <tero.saarni@est.tech>
2024-05-31 21:46:57 +0300

    Add limited support for LDAP password policy control
    Send password policy control during LDAP bind operation and receive response
    according to chapter 9.1 of [1].  When server responds with error
    "changeAfterReset" then prompt user to update their password by adding
    UPDATE_PASSWORD required action
    
    Following preconditions need to be met:
    
    - import mode enabled, otherwise required actions cannot be persisted.
    - edit mode is set writable, so that users can modify their passwords.
    
    Without these preconditions changeAfterReset is treated as failed
    authentication.
    
    [1] https://datatracker.ietf.org/doc/html/draft-behera-ldap-password-policy-11
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- js/apps/admin-ui/src/user-federation/ldap/LdapSettingsAdvanced.tsx
+++ js/apps/admin-ui/src/user-federation/ldap/LdapSettingsAdvanced.tsx
@@ -123,6 +123,36 @@ export const LdapSettingsAdvanced = ({
         </FormGroup>
 
         <FormGroup
+          label={t("enableLdapPasswordPolicy")}
+          labelIcon={
+            <HelpItem
+              helpText={t("user-federation-help:enableLdapPasswordPolicyHelp")}
+              fieldLabelId="user-federation:enableLdapPasswordPolicy"
+            />
+          }
+          fieldId="kc-enable-ldap-password-policy"
+          hasNoPaddingTop
+        >
+          <Controller
+            name="config.enableLdapPasswordPolicy"
+            defaultValue={["false"]}
+            control={form.control}
+            render={({ field }) => (
+              <Switch
+                id={"kc-enable-ldap-password-policy"}
+                data-testid="ldap-password-policy"
+                isDisabled={false}
+                onChange={(value) => field.onChange([`${value}`])}
+                isChecked={field.value[0] === "true"}
+                label={t("common:on")}
+                labelOff={t("common:off")}
+                aria-label={t("enableLdapPasswordPolicy")}
+              />
+            )}
+          ></Controller>
+        </FormGroup>
+
+        <FormGroup
           label={t("trustEmail")}
           labelIcon={
             <HelpItem
