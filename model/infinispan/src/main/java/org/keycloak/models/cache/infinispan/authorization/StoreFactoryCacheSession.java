7a17ef7ba4321697209538dc125aee61139cf10f
Stefan Guilhen <sguilhen@redhat.com>
2026-01-19 10:11:42 -0300

    Prevent NPE when evaluating policies and policy is deleted
    Closes #45561
    
    Signed-off-by: Stefan Guilhen <sguilhen@redhat.com>

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/authorization/StoreFactoryCacheSession.java
+++ model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/authorization/StoreFactoryCacheSession.java
@@ -1094,7 +1094,9 @@ public class StoreFactoryCacheSession implements CachedStoreFactoryProvider {
 
                 if (consumer != null) {
                     for (String id : policies) {
-                        consumer.accept((R) findById(resourceServer, id));
+                        Policy policy = findById(resourceServer, id);
+                        if (policy != null)
+                            consumer.accept((R) policy);
                     }
                 } else {
                     model = policies.stream().map(resourceId -> (R) findById(resourceServer, resourceId))
