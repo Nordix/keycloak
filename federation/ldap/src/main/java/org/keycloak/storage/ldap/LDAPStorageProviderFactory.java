c2a239683fc52e3cfbfc773afc6618f7741ff0c0
Tero Saarni <tero.saarni@est.tech>
2026-02-23 15:05:48 +0200

    Add limited support for LDAP password policy control
    Send password policy control during LDAP bind operation and receive response
    according to chapter 9.1 of [1].  When server responds with error
    "changeAfterReset" then prompt user to update their password by adding
    UPDATE_PASSWORD required action
    
    Following preconditions need to be met:
    
    - import mode enabled, otherwise required actions cannot be persisted.
    - edit mode is set writable, so that users can modify their passwords.
    
    Without these preconditions changeAfterReset is treated as failed
    authentication.
    
    [1] https://datatracker.ietf.org/doc/html/draft-behera-ldap-password-policy-11
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- federation/ldap/src/main/java/org/keycloak/storage/ldap/LDAPStorageProviderFactory.java
+++ federation/ldap/src/main/java/org/keycloak/storage/ldap/LDAPStorageProviderFactory.java
@@ -223,6 +223,9 @@ public class LDAPStorageProviderFactory implements UserStorageProviderFactory<LD
                 .type(ProviderConfigProperty.BOOLEAN_TYPE)
                 .defaultValue("false")
                 .add()
+                .property().name(LDAPConstants.ENABLE_LDAP_PASSWORD_POLICY)
+                .defaultValue("false")
+                .add()
                 .build();
     }
 
